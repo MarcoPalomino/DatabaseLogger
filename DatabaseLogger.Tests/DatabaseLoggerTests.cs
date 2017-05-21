42915488566da2eac4a27ca1fe94faff0aced673
MarcoPalomino <palomino.marco@pucp.edu.pe>
2017-05-21 17:37:39 -0500

    Agregar archivos de proyecto.

---
+++ DatabaseLogger.Tests/DatabaseLoggerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseLogger.Tests
+{
+    [TestClass]
+    public class DatabaseLoggerTests
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void VerifyConnectionStringIsObtained()
+        {
+            //Arrange
+            var logger = new Logger();
+
+            //Act
+            var connectionString = logger.GetConnectionString();
+
+            //Assert
+            Assert.IsTrue(!string.IsNullOrEmpty(connectionString));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void VerifyDBLogWasSuccessfullySaved()
+        {
+            //Arrange
+            var logger = new Logger();
+            string logType = "W";
+            string logMessage = "log Message";
+
+            //Act
+            bool isProcessedSuccessfully;
+            isProcessedSuccessfully = logger.DoLog(logType, logMessage);
+
+            //Assert
+            Assert.IsTrue(isProcessedSuccessfully);
+        }
+    }
+}
---
+++ DatabaseLogger/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DatabaseLogger
+{
+    public class Logger : ILogger
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetConnectionString()
+        {
+            ConnectionStringSettingsCollection connectionStrings = ConfigurationManager.ConnectionStrings;
+            var connectionString = connectionStrings[1].ToString();
+            return connectionString;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="storedProcedureName"></param>
+        /// <param name="logType"></param>
+        /// <param name="logMessage"></param>
+        /// <returns></returns>
+        public bool DoLog(string logType, string logMessage)
+        {
+            var linesAffected = 0;
+            try
+            {
+                using (SqlConnection sqlConn = new SqlConnection(GetConnectionString()))
+                {
+                    sqlConn.Open();
+
+                    using (SqlCommand sqlCommand = new SqlCommand("SPU_INS_DB_LOGGER", sqlConn))
+                    {
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                        sqlCommand.Parameters.Add("@LOG_TYPE", SqlDbType.VarChar, 50).Value = logType;
+                        sqlCommand.Parameters.Add("@LOG_MESSAGE", SqlDbType.VarChar, 50).Value = logMessage;
+
+                        linesAffected = sqlCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+
+            }
+            finally
+            {
+                //Final notes
+            }
+            return linesAffected > 0;
+        }
+    }
+}
